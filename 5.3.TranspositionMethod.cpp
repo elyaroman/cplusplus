59e480f94406c6167c53f07c8a82adce0afcaa09
elyaroman <90469789+elyaroman@users.noreply.github.com>
2023-09-24 21:02:00 +0300

    fifth practical exercise

---
+++ 5.3.TranspositionMethod.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+int* transposition(int array[], int n, int key)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (array[i] == key)
+        {
+            int temp = array[i];
+            array[i] = array[i - 1];
+            array[i - 1] = temp;
+            return array;
+        }
+    }
+}
+
+
+int main()
+{
+    int n, key;
+    cin >> n;
+    cin >> key;
+
+    int* array = new int[n];
+    for (int i = 0; i < n; i++)
+        cin >> array[i];
+    int* c = transposition(array, n, key);
+
+    for (int i = 0; i < n; i++)
+        cout << c[i] << " ";
+    cout << endl;
+    delete[]c;
+}
