66816579f951f86a24008557d6d58211a9b6bf24
elyaroman <90469789+elyaroman@users.noreply.github.com>
2023-09-24 17:22:53 +0300

    fourth practical exercise

---
+++ 4.1.QuadraticEquation.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+int Myroot(double a, double b, double c, double &x1, double &x2)
+{
+    double d = b * b - 4 * a * c;
+    if (d < 0)
+        return -1;
+    else
+    {
+        x1 = x1 + (-b + pow(d, 1.0 / 2)) / (2 * a);
+        x2 = x2 + (-b + pow(b * b + 4 * a * c, 1.0 / 2)) / (2 * a);
+        return 1;
+    }
+}
+
+int main()
+{
+    double a, b, c;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+
+    double xf = 0, xs = 0;
+    int result = Myroot(a, b, c, xf, xs);
+
+    if (result == 1)
+        cout << "x1 = " << xf << " and x2 = " << xs;
+    else
+        cout << "no roots";
+
+    return 0;
+}
---
+++ 4.2.Input.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+bool Input(char &a, char &b)
+{
+	cin >> a;
+	cin >> b;
+
+	if (isdigit(a) && isdigit(b))
+		return true;
+	else
+		return false;
+}
+
+int main()
+{
+	char a, b;
+
+	if (Input(a, b) == false)
+	{
+		cerr << "error";
+		return 1;
+	}
+
+	int s = (a - '0') + (b - '0');
+	cout << s;
+	return 0;
+}
