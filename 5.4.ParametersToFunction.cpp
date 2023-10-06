c515790384b2165d7314ca3f09ea906efdeaa897
elyaroman <90469789+elyaroman@users.noreply.github.com>
2023-10-06 16:56:05 +0300

    fifth practical exercise

---
+++ 5.4.ParametersToFunction.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	if (argc < 4) {
+		cout << "error" << endl;
+		return 1;
+	}
+	int result = 0;
+	if (_stricmp(argv[1], "-a") == 0) {
+		result = atoi(argv[2]) + atoi(argv[3]);
+	}
+	else if (_stricmp(argv[1], "-m") == 0) {
+		result = atoi(argv[2]) * atoi(argv[3]);
+	}
+	else {
+		cout << "error" << endl;
+		return 1;
+	}
+	cout << result;
+	return 0;
+}
