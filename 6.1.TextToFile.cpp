8122199485b7c7b972b1ff08ed6f4610a26a27d4
elyaroman <90469789+elyaroman@users.noreply.github.com>
2023-10-06 14:57:22 +0300

    sixth practical exercise

---
+++ 6.1.TextToFile.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <string>
+using namespace std;
+
+int main()
+{
+    system("chcp 1251");
+
+    int n;
+    string line;
+    cout << "Enter the length of the poem\n";
+    cin >> n;
+    cin.ignore();
+    ofstream fout("text.txt");
+    for (int i = 0; i < n; i++)
+    {
+        getline(cin, line);
+        fout << line << endl;
+    }
+    fout.close();
+}
---
+++ 6.2.DataToFile.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+
+int* max_vect(int kc, int a[], int b[])
+{
+    int* c = new int[kc];
+    for (int i = 0; i < kc; i++)
+    {
+        if (a[i] > b[i])
+            c[i] = a[i];
+        else
+            c[i] = b[i];
+    }
+    return c;
+}
+
+int main()
+{
+    int a[] = { 1,2,3,4,5,6,7,2 };
+    int b[] = { 7,6,5,4,3,2,1,3 };
+    int kc = sizeof(a) / sizeof(a[0]);
+    int* c;
+    c = max_vect(kc, a, b);
+
+    ofstream fout("test.txt");
+    for (int i = 0; i < 8; i++)
+    {
+        fout << "a[" << i << "] = " << a[i] << "; ";
+        fout << "b[" << i << "] = " << b[i] << "; ";
+        fout << "c[" << i << "] = " << c[i] << endl;
+    }
+    fout.close();
+
+    delete[]c;
+}
